a5808404f867bd9019fc63759bf671f322c72519
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-31 16:36:39 +0300

    ExchangeRate Entity Added

---
+++ Core/DataAPI.Domain/Entities/ExchangeRate.cs
@@ -0,0 +1,18 @@
+﻿using DataAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAPI.Domain.Entities
+{
+    public class ExchangeRate:BaseEntity
+    {
+        public string ExchangeType { get; set; }
+        public decimal ForexBuying { get; set; }
+        public decimal ForexSelling { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
