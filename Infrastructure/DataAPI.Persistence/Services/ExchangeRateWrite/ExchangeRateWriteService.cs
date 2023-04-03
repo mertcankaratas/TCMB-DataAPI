d0ed29fe0d92eb2ae40cf544de168980a2dea072
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-04-03 17:18:03 +0300

    package setup

---
+++ Infrastructure/DataAPI.Persistence/Services/ExchangeRateWrite/ExchangeRateWriteService.cs
@@ -0,0 +1,35 @@
+﻿using DataAPI.Application.Abstraction.Services.ExchangeRateRead;
+using DataAPI.Application.Abstraction.Services.ExchangeRateWrite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAPI.Persistence.Services.ExchangeRateWrite
+{
+    public class ExchangeRateWriteService : IExchangeRateWriteService
+    {
+        readonly ITCMBExchangeRateService _exchangeRateService;
+
+        public ExchangeRateWriteService(ITCMBExchangeRateService exchangeRateService)
+        {
+            _exchangeRateService = exchangeRateService;
+        }
+
+        public Task SetExchangeCrossRateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetExchangeEffectiveRateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetExchangeRateItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
